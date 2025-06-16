4a5067df6044093e1b3baa99346a9d79be26f3de
Bruno Bartol <bruno.bartol1609@gmail.com>
2025-06-16 16:28:38 +0200

    added cocktail details screen

---
+++ Cocktails/Cocktails/UI/Features/CocktailDetails/CocktailDetailsView.swift
@@ -0,0 +1,95 @@
+import SwiftUI
+import OrderedCollections
+
+struct CocktailDetailsView: View {
+    @ObservedObject private var viewModel: CocktailDetailsVM
+    
+    init(viewModel: CocktailDetailsVM) {
+        self.viewModel = viewModel
+    }
+    
+    private var divider: some View {
+        Rectangle()
+            .frame(height: Constants.dividerHeight)
+            .foregroundStyle(Color.lightSky)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                if let urlString = viewModel.state.cocktail?.imageUrl,
+                   let url = URL(string: urlString) {
+                    ZStack {
+                        AsyncImage(url: url, content: {
+                            $0.resizable()
+                                .scaledToFit()
+                        }, placeholder: {
+                            ProgressView()
+                                .scaledToFit()
+                        })
+                    }
+                }
+                
+                Spacer()
+            }
+            
+            VStack(alignment: .leading, spacing: Constants.titleSpacing) {
+                Text(viewModel.state.cocktail?.title ?? "")
+                    .appFont(size: Constants.fontSize, foregroundColor: .white, weight: .black)
+                    .padding(.leading, Constants.padding)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: Constants.spacing) {
+                        Header(category: viewModel.state.cocktail?.category ?? "",
+                               glass: viewModel.state.cocktail?.glass ?? "",
+                               alcoholicType: viewModel.state.cocktail?.alcoholic ?? "")
+                        
+                        divider
+                        
+                        IngredientsSection(ingredients: viewModel.state.cocktail?.ingredients ?? [],
+                                           measures: viewModel.state.cocktail?.measures ?? [])
+                        
+                        divider
+                        
+                        DirectionsSection(instructions: viewModel.state.cocktail?.instructions ?? "")
+                        
+                        divider
+                        
+                        LastModifiedFooter(date: viewModel.formatDate())
+                    }
+                }
+                .padding(.top, Constants.padding)
+                .safeAreaPadding(.bottom, Constants.padding)
+                .containerRelativeFrame(.vertical) { width, axis in
+                    width * Constants.contentRatio
+                }
+                .background(Color.white)
+                .clipShape(UnevenRoundedRectangle(topLeadingRadius: Constants.cornerRadius,
+                                                  topTrailingRadius: Constants.cornerRadius))
+            }
+            
+            if viewModel.state.isLoading {
+                ProgressView()
+            }
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+}
+
+fileprivate struct Constants {
+    private init() {}
+    
+    static let placeHolderImage = "wineglass.fill"
+    
+    static let dividerHeight: CGFloat = 1
+    static let spacing: CGFloat = 20
+    static let titleSpacing: CGFloat = 37
+    static let padding: CGFloat = 20
+    static let fontSize: CGFloat = 24
+    static let cornerRadius: CGFloat = 40
+    static let contentRatio: CGFloat = 0.7
+}
+
+#Preview {
+    CocktailDetailsView(viewModel: CocktailDetailsVM(id: "1100", service: MockCocktailDetailsService()))
+}
