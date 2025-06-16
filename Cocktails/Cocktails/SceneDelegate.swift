4bdd50e81cb11c1239243512bd64e05f77f571a6
Bruno Bartol <bruno.bartol1609@gmail.com>
2025-06-16 16:25:56 +0200

    added navigation

--- Cocktails/Cocktails/SceneDelegate.swift
+++ Cocktails/Cocktails/SceneDelegate.swift
@@ -12,19 +12,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
+    private let navigationController = UINavigationController()
+    private var coordinator: Coordinator?
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let rootView = CocktailList(viewModel: CocktailListViewModel(service: CocktailsListService()))
-        let viewController = UIHostingController(rootView: rootView)
-        
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = viewController
+            window.rootViewController = navigationController
             self.window = window
+            
+            coordinator = Coordinator(navigationController: navigationController)
+            coordinator?.start()
+            
             window.makeKeyAndVisible()
         }
     }
